31ada3745f1a4558c4906694e80b4867f3dc1b5c
sammy paiz <sammypaiz@sammypaiz-2.local>
2019-01-24 22:57:15 -0800

    json.parse()

--- js/javascript.js
+++ js/javascript.js
@@ -1,9 +1,14 @@
+var output = document.querySelector('#output');
 var xhr = new XMLHttpRequest();
-var url = 'http://api.icndb.com/jokes/random';
+var url = 'https://api.chucknorris.io/jokes/random';
 xhr.onreadystatechange = function() {
 	console.log(xhr.readyState);
-	if(xhr.readyState == 4) {
-		console.log(xhr.response);
+	if(xhr.readyState == 4 && xhr.status == 200) {
+		console.log(xhr.responseText);
+		var str = xhr.responseText;
+		var obj = JSON.parse(str);
+		output.innerHTML = obj.value + '<br><img src="' + obj.icon_url + '">';
+		console.log(obj);
 	}
 }
 xhr.open('GET', url);
