f576be968bf3f34928a9f43cc3bed9fc6b18fa88
sammy paiz <sammypaiz@sammypaiz-2.local>
2019-01-30 10:19:50 -0800

    country app complete

--- js/javascript.js
+++ js/javascript.js
@@ -134,15 +134,38 @@ const url = 'https://restcountries.eu/rest/v2/all';
 // 	})
 // }
 
+
+
 //country function
 
+// let myData = {};
+// fetch(url)
+// .then(function(res) {
+// 	return res.json();
+// })
+// .then(function(data) {
+// 	myData = data;
+// 	buildSelect(data);
+// })
+
+// function buildSelect(d) {
+// 	let select = document.createElement('select');
+// 	d.forEach(function(item, index) {
+// 		let option = document.createElement('option');
+// 		console.log(item, index);
+// 		option.value = index;
+// 		option.textContent = item.name;
+// 		select.appendChild(option);
+// 	})
+// 	document.querySelector('body').appendChild(select);
+// }
 let myData = {};
 fetch(url)
 .then(function(res) {
 	return res.json();
 })
 .then(function(data) {
-	myData = data;
+	myData = data;		
 	buildSelect(data);
 })
 
@@ -155,8 +178,25 @@ function buildSelect(d) {
 		option.textContent = item.name;
 		select.appendChild(option);
 	})
+	select.addEventListener('change', outputData);
 	document.querySelector('body').appendChild(select);
 }
+
+function outputData(e) {
+	console.log(e);
+	console.log(e.target.value);
+	console.log(myData[e.target.value]);
+	let obj = myData[e.target.value];
+	output.innerHTML = '<h1>' + obj.name + '</h1>';
+	output.innerHTML += '<p>Region: ' + obj.region + '</p>';
+	document.querySelector('img').src = obj.flag;
+}
+
+
+
+
+
+
 
 
 
